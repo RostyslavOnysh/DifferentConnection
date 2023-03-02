24e6408b6d0dc7043c4a1fa0732d86f0c3a89194
Rostyslav <98691406+RostyslavOnysh@users.noreply.github.com>
2023-03-02 16:50:30 +0000

    Add files via upload

--- Connection2/Connection2.java
+++ Connection2/Connection2.java
@@ -9,21 +9,51 @@ import java.sql.Statement;
 
 public class Connection2 {
     public static void main(String[] args) {
-        Class<Driver>driverClass = Driver.class;
-        // create new database
+        Class<Driver> driverClass = Driver.class;
+        /* create new database
         String sql = """
-                CREATE TABLE IF NOT EXISTS info(
+               CREATE TABLE IF NOT EXISTS info(
                 id SERIAL PRIMARY KEY ,
                 data TEXT NOT NULL);
-                """;
+               """; */
 
+
+                                           // insert
+               /*   String sql = """
+                    INSERT INTO info (data)
+                    VALUES
+                    ('Test1'),
+                     ('Test2'),
+                      ('Test3'),
+                        ('Test4')
+                                   """;
+                */
+
+                /*     String sql = """
+                        UPDATE info
+                         SET data = 'TestTest'
+                           WHERE id = 4
+                                           """;
+                 */
+
+        String sql = """
+                SELECT * FROM testbase
+
+                """;
 
         try {
             Connection connection = ConnectionManager.open();
             //открываю statement после connection.метод create statement + try catch
             var statement = connection.createStatement();
             System.out.println(connection.getTransactionIsolation());
-             var executeResult = statement.execute(sql);   //for ddl operation
+            var executeResult = statement.executeQuery(sql);
+            // var executeResult = statement.execute(sql);   //for ddl operation
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("name"));
+                System.out.println(executeResult.getString("value"));
+                System.out.println("---------");
+            }
             System.out.println(executeResult);
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -31,4 +61,10 @@ public class Connection2 {
 
     }
 }
-//////
+
+
+// через (execute) только true or false;
+//есть метод для возврата количества обновленных строк
+// sout(statement.getUpdateCount());
+// ResultSet похож на iterator
+//шоб вывести результат выборки нада юзать цикл while (executeResult.next()){sout(executeResult.getLong(id))}
